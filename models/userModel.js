d66b2e3022ef2b968aa0f8ac526a29f79f9d3c82
amanuela97 <babyzewdie@gmail.com>
2019-11-22 11:40:02 +0200

    final commit

--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,41 @@
 'use strict';
+const pool = require('../database/db');
+const promisePool = pool.promise();
+
+const getAllUsers = async () => {
+  try {
+    const [rows] = await promisePool.execute('SELECT * FROM wop_user');
+    return rows;
+  } catch (e) {
+    console.log('error', e.message);
+    return {error: 'error in database query'};
+  }
+};
+
+const getUser = async (params) => {
+  try {
+    const [rows] = await promisePool.execute(
+        'SELECT * FROM wop_user WHERE user_id = ?;',
+        params,
+    );
+    return rows;
+  } catch (e) {
+    console.log('error', e.message);
+    return {error: 'error in database query'};
+  }
+};
+
+module.exports = {
+  getAllUsers,
+  getUser,
+};
+
+
+
+
+
+
+/*'use strict';
 const users = [
   {
     id: '1',
@@ -16,4 +53,4 @@ const users = [
 
 module.exports = {
   users,
-};
+};*/

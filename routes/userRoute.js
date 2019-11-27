db1a07e4e58af944d7648f6307f513d8921acb1c
amanuela97 <babyzewdie@gmail.com>
2019-11-27 12:44:36 +0200

    new branch

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,7 +1,12 @@
 'use strict';
-const express = require('express');
+
+const express = require ('express');
 const router = express.Router();
-const userController = require('../controllers/userController');
+const userController = require('../controllers/userController.js');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
+
+
 
 router.get('/', userController.user_list_get);
 
@@ -9,12 +14,12 @@ router.get('/:id', userController.user_get);
 
 router.post('/', userController.user_create_post);
 
-router.put('/', (req, res) => {
-    res.send('With this endpoint you can modify users.');
+router.put('/', (req, res) =>{
+    res.send("With this endpoint you can edit users.");
 });
 
-router.delete('/', (req, res) => {
-    res.send('With this endpoint you can delete users.');
+router.delete('/', (req, res) =>{
+    res.send("With this endpoint you can delete users.");
 });
 
 module.exports = router;
